1a1210bcc779893c7c183490185474df4e8b8971
Merlin <merlin.p.varghese@gmail.com>
2019-08-27 12:20:01 +0530

    adding .gitignore and GreeterTest.java

--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 public class GreeterTest {
 
+
     @Test
     public void testThatGreeterReturnsTheCorrectGreeting() {
+        assertEquals("Hello, World!", new Greeter().getGreeting());
+    }
+
+    @Test
+    public void test1ThatGreeterReturnsTheCorrectGreeting() {
         assertEquals("Hello, World!", new Greeter().getGreeting());
     }
 
